d8614671bd4c58dbfac5ab8b119c0d1cf0a62530
Max Verevkin <34583604+MaxVerevkin@users.noreply.github.com>
2024-04-29 04:36:05 +0300

    remove some dependencies (#17)
    * Remove futures-executor by switching to tokio's channel
    
    * use futures-util directly

--- lib/src/watcher.rs
+++ lib/src/watcher.rs
@@ -1,7 +1,7 @@
-pub(crate) use futures::channel::mpsc::Receiver;
-use futures::{channel::mpsc::channel, SinkExt};
 use notify::{Config, Event, RecursiveMode, Result};
 pub(crate) use notify::{INotifyWatcher, PollWatcher, Watcher};
+use tokio::sync::mpsc::channel;
+pub(crate) use tokio::sync::mpsc::Receiver;
 
 use std::path::Path;
 pub use std::time::Duration;
@@ -10,30 +10,22 @@ pub fn pseudo_fs_watcher<P: AsRef<Path>>(
     path: P,
     poll_interval: Duration,
 ) -> Result<(PollWatcher, INotifyWatcher, Receiver<Result<Event>>)> {
-    let (mut tx, rx) = channel(1);
+    let (tx, rx) = channel(1);
 
     let config = Config::default()
         .with_compare_contents(true) // crucial part for pseudo filesystems
         .with_poll_interval(poll_interval);
 
     // PollWatcher is used to observe the devices as they come/go
-    let mut poll_tx = tx.clone();
+    let poll_tx = tx.clone();
     let mut poll_watcher = PollWatcher::new(
-        move |res: notify::Result<Event>| {
-            futures::executor::block_on(async {
-                poll_tx.send(res).await.unwrap();
-            });
-        },
+        move |res: notify::Result<Event>| poll_tx.blocking_send(res).unwrap(),
         config,
     )?;
 
     // INotifyWatcher watches the contents of the files
     let inotify_watcher = INotifyWatcher::new(
-        move |res: notify::Result<Event>| {
-            futures::executor::block_on(async {
-                tx.send(res).await.unwrap();
-            });
-        },
+        move |res: notify::Result<Event>| tx.blocking_send(res).unwrap(),
         config,
     )?;
 
